93ce08127d53b31dfaad6398f9091a70a33b91f1
Mike <mikesmallhelp@gmail.com>
2023-11-14 17:47:51 +0200

    TestCommand

--- src/commands/test/index.ts
+++ src/commands/test/index.ts
@@ -1,7 +1,7 @@
 import {Args, Command, Flags} from '@oclif/core'
 import { exec } from 'child_process';
 
-export default class Hello extends Command {
+export default class TestCommand extends Command {
   static args = {
     url: Args.string({description: 'Application url to test, for example: http://localhost:3000', required: true}),
   }
@@ -17,7 +17,7 @@ export default class Hello extends Command {
   }
 
   async run(): Promise<void> {
-    const {args, flags} = await this.parse(Hello)
+    const {args, flags} = await this.parse(TestCommand)
 
     exec(`ROOT_URL='${args.url}' npx playwright test --project=chromium`, (error, stdout, stderr) => {
       if (error) {
