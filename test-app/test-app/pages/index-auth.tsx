9cab5b33d104eac154e2640350a8ef5764121255
Mike <mikesmallhelp@gmail.com>
2023-12-04 17:50:49 +0200

    Yksinkertainen autentikointi

---
+++ test-app/test-app/pages/index-auth.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import FrontPage from '../components/FrontPage';
+
+const IndexComponent = () => {
+  const [authenticated, setAuthenticated] = React.useState(false);
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [errorText, setErrorText] = React.useState("");
+
+  const handleLogin = async (e: React.SyntheticEvent) => {
+    if (username == "aku" && password == "ankka") {
+      setAuthenticated(true);
+    } else {
+      setErrorText("Wrong username or password!");
+    }
+  }
+
+  return (
+    <>
+      {authenticated ? (
+        <FrontPage pageDirectory='working-page2' />
+      ) : (
+        <>
+          <label>
+            Username:
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </label>
+          <label>
+            Password:
+            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </label>
+          <button onClick={handleLogin}>Login</button>
+          <p>{errorText}</p>
+        </>
+      )}
+    </>
+  );
+};
+
+export default IndexComponent;
