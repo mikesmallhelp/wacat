04aafc3dc8e8b0d17f98291830fc1da927406291
Mike <mikesmallhelp@gmail.com>
2023-12-12 17:43:10 +0200

    testiin muutos

--- e2e-tests/test.spec.ts
+++ e2e-tests/test.spec.ts
@@ -38,7 +38,7 @@ test('test an application', async ({ page }) => {
         await authenticate({ authenticationConfiguration, page });
     }
 
-    await handlePage({ authenticationConfiguration, page });
+    await handlePage({ page, authenticationConfiguration });
 });
 
 export const authenticate = async ({ authenticationConfiguration, page }:
@@ -70,8 +70,8 @@ export const authenticate = async ({ authenticationConfiguration, page }:
     console.log('Filled the username and the password. Pushed the authentication button');
 }
 
-const handlePage = async ({ authenticationConfiguration, page }:
-    { authenticationConfiguration: AuthenticationConfiguration, page: Page }) => {
+const handlePage = async ({ page, authenticationConfiguration }:
+    { page: Page, authenticationConfiguration?: AuthenticationConfiguration }) => {
     console.log('In the page: ' + page.url());
 
     await page.waitForTimeout(1000);
@@ -79,7 +79,7 @@ const handlePage = async ({ authenticationConfiguration, page }:
 
     await checkPageForErrors({ page });
     await fillInputsAndSelectFromDropDownListsAndClickButtons({ page });
-    await visitLinks({ authenticationConfiguration, page });
+    await visitLinks({ page, authenticationConfiguration });
 }
 
 const checkPageForErrors = async ({ page }: { page: Page }) => {
@@ -134,8 +134,9 @@ const selectFromDropDownLists = async ({ page }: { page: Page }) => {
     }
 }
 
-const visitLinks = async ({ authenticationConfiguration, page }: { authenticationConfiguration: AuthenticationConfiguration, page: Page }) => {
-    const links = await page.locator('a').evaluateAll((links: HTMLAnchorElement[], authConfig: AuthenticationConfiguration) => {
+const visitLinks = async ({ page, authenticationConfiguration }:
+                          { page: Page, authenticationConfiguration?: AuthenticationConfiguration }) => {
+    const links = await page.locator('a').evaluateAll((links: HTMLAnchorElement[], authConfig?: AuthenticationConfiguration) => {
         if (authConfig && authConfig.noLogoutLinkOrButtonName) {
             return links
                 .filter((link) => link.textContent && link.textContent.trim() !== 'Logout')
@@ -148,7 +149,7 @@ const visitLinks = async ({ authenticationConfiguration, page }: { authenticatio
     for (const link of links) {
         if (!visitedUrls.includes(link) && hostIsSame({ rootUrl, url: link })) {
             await page.goto(link);
-            await handlePage({ authenticationConfiguration, page });
+            await handlePage({ page, authenticationConfiguration });
         }
     }
 }
