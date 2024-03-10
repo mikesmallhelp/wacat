9de8d43fcaa617c358b722ecea2cd6123020654c
Mike <mikesmallhelp@gmail.com>
2024-03-10 10:38:03 +0200

    Add the checkbox

--- test-app/test-app/pages/input-field-and-button-push-causes-error/index.tsx
+++ test-app/test-app/pages/input-field-and-button-push-causes-error/index.tsx
@@ -6,15 +6,25 @@ const IndexComponent = () => {
   const [loading, setLoading] = useState(true);
   const [errorMessage, setErrorMessage] = React.useState('');
   const [inputValue, setInputValue] = React.useState('');
+  const [optionValue, setOptionValue] = React.useState('');
+  const [checkbox, setCheckbox] = useState(false);
 
   const handleInputValueChange = (event: ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
   };
 
+  const handleOptionValueChange = async (event: ChangeEvent<HTMLSelectElement>) => {
+    setOptionValue(event.target.value);
+  };
+
+  const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCheckbox(event.target.checked);
+  };
+
   const handleButtonClick = async () => {
     await fetchData('200', setLoading);
     
-    if (inputValue) {
+    if (inputValue && optionValue && checkbox) {
       setErrorMessage('Error occurred!');
     }
   }
@@ -26,6 +36,21 @@ const IndexComponent = () => {
       <input />
 
       <input onChange={handleInputValueChange} />
+
+      <select name="someValues" id="someValues">
+        <option value="aaa">aaa</option>
+        <option value="bbb">bbb</option>
+      </select>
+
+      <select onChange={handleOptionValueChange} name="someValues2" id="someValues2">
+        <option value="aaa">aaa</option>
+        <option value="bbb">bbb</option>
+      </select>
+
+      <div>
+        <input type="checkbox" />
+        <input type="checkbox" checked={checkbox} onChange={handleCheckboxChange} /> Second Checkbox
+      </div>
 
       <button onClick={handleButtonClick}>Button</button>
       
