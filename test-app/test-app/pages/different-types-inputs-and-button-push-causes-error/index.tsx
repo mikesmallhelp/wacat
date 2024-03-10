4372ac2b1f5c2ab57c56c612a771acafe9a0ec46
Mika Tapanainen <63582395+mikesmallhelp@users.noreply.github.com>
2024-03-10 13:17:49 +0200

    Add checkbox (#23)
    * Add the checkbox
    
    * Test passing
    
    * Refactoring
    
    * Correct order in run tests
    
    * Poistettu ylimääräisiä testejä
    
    * Remove not needed pages
    
    * With basic input check that there is not type attribute, because for example date type causes exception

--- test-app/test-app/pages/different-types-inputs-and-button-push-causes-error/index.tsx
+++ test-app/test-app/pages/different-types-inputs-and-button-push-causes-error/index.tsx
@@ -5,16 +5,26 @@ import { fetchData } from '../../util/util';
 const IndexComponent = () => {
   const [loading, setLoading] = useState(true);
   const [errorMessage, setErrorMessage] = React.useState('');
+  const [inputValue, setInputValue] = React.useState('');
   const [optionValue, setOptionValue] = React.useState('');
+  const [checkbox, setCheckbox] = useState(false);
+
+  const handleInputValueChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
 
   const handleOptionValueChange = async (event: ChangeEvent<HTMLSelectElement>) => {
     setOptionValue(event.target.value);
   };
 
+  const handleCheckboxChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCheckbox(event.target.checked);
+  };
+
   const handleButtonClick = async () => {
     await fetchData('200', setLoading);
-    
-    if (optionValue) {
+
+    if (inputValue && optionValue && checkbox) {
       setErrorMessage('Error occurred!');
     }
   }
@@ -22,6 +32,10 @@ const IndexComponent = () => {
   const PageContent = (
     <div>
       <div>{errorMessage}</div>
+
+      <input />
+
+      <input onChange={handleInputValueChange} />
 
       <select name="someValues" id="someValues">
         <option value="aaa">aaa</option>
@@ -33,18 +47,20 @@ const IndexComponent = () => {
         <option value="bbb">bbb</option>
       </select>
 
+      <input type="checkbox" />
+      <input type="checkbox" checked={checkbox} onChange={handleCheckboxChange} />
+
       <button onClick={handleButtonClick}>Button</button>
-      
+
       <a href='/'>Back to main page</a>
     </div>
   );
 
   return (
     <>
-      <PageTemplate title={'Test page - drop-down-list-selection-and-button-push-causes-error'} content={PageContent} loading={loading} setLoading={setLoading} />
+      <PageTemplate title={'Test page - input-field-and-button-push-causes-error'} content={PageContent} loading={loading} setLoading={setLoading} />
     </>
   );
 };
 
 export default IndexComponent;
-
