ed76bd3df19d397ac613f472dc984dfa8851ad56
Mike <mikesmallhelp@gmail.com>
2024-03-19 18:57:56 +0200

    Updated Windows tests

--- run-manual-tests.bat
+++ run-manual-tests.bat
@@ -1,10 +1,13 @@
-call wacat test --debug --headless https://mikesmallhelp-test-application.vercel.app
-call wacat test --debug --wait 1000 https://mikesmallhelp-test-application-http-500-error.vercel.app/
-call wacat test --debug --conf example-files/configuration-error-texts.json --wait 1000 https://mikesmallhelp-test-application-error-in-page.vercel.app/
-call wacat test --debug --input-texts example-files/input-texts.txt --wait 1000 https://mikesmallhelp-test-application.vercel.app/
-call wacat test --debug --input-texts https://raw.githubusercontent.com/mikesmallhelp/wacat/main/example-files/input-texts.txt --wait 1000 https://mikesmallhelp-test-application.vercel.app/
-call wacat test --debug --only-links --wait 1000 https://mikesmallhelp-test-application.vercel.app/
-call wacat test --debug --conf example-files/configuration-authentication.json https://mikesmallhelp-test-application-simple-authentication.vercel.app/
-call wacat test --debug --conf example-files/configuration-complicated-authentication.json https://mikesmallhelp-test-application-more-complicated-authentication.vercel.app/
-call wacat test --debug --conf example-files/configuration-complicated-authentication-with-not-visit-link-urls-remote.json https://mikesmallhelp-test-application-more-complicated-authentication.vercel.app/
-call wacat test --debug --timeout 1 https://mikesmallhelp-test-application.vercel.app
+call wacat test --wait 5000 --headless https://mikesmallhelp-test-application.vercel.app
+call wacat test --wait 5000 https://mikesmallhelp-test-application-http-500-error.vercel.app/
+call wacat test --wait 5000 --conf example-files/configuration-error-texts.json https://mikesmallhelp-test-application-error-in-page.vercel.app/
+call wacat test --wait 5000 https://mikesmallhelp-test-application-error-in-browser-console.vercel.app
+call wacat test --wait 5000 --random-input-texts-min-length 1 --random-input-texts-max-length 3 --random-input-texts-charset ®©¥¬¿ https://mikesmallhelp-test-application.vercel.app/
+call wacat test --wait 5000 --input-texts example-files/input-texts.txt https://mikesmallhelp-test-application.vercel.app/
+call wacat test --wait 5000 --input-texts https://raw.githubusercontent.com/mikesmallhelp/wacat/main/example-files/input-texts.txt https://mikesmallhelp-test-application.vercel.app/
+call wacat test --wait 5000 --only-links https://mikesmallhelp-test-application.vercel.app/
+call wacat test --wait 5000 --conf example-files/configuration-authentication.json https://mikesmallhelp-test-application-simple-authentication.vercel.app/
+call wacat test --wait 5000 --conf example-files/configuration-complicated-authentication.json https://mikesmallhelp-test-application-more-complicated-authentication.vercel.app/
+call wacat test --wait 5000 --conf example-files/configuration-complicated-authentication-with-not-visit-link-urls-remote.json https://mikesmallhelp-test-application-more-complicated-authentication.vercel.app/
+call wacat test --timeout 1 https://mikesmallhelp-test-application.vercel.app
+call wacat test --help
