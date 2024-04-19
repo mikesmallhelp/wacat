38d6f37ec9f7e1f632b404b689a8e7726a8cfe51
Mike <mikesmallhelp@gmail.com>
2024-04-19 19:00:37 +0300

    retries 2 in CI, because of mac problems

--- playwright.config.ts
+++ playwright.config.ts
@@ -53,7 +53,7 @@ export default defineConfig({
   ],
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'line',
-  retries: 0,
+  retries: 2,
   testDir: './e2e-tests',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
