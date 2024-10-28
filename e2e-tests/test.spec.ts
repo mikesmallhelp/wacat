570725731c89edf4ecee0ec626e8bb9ed2b14188
Mike <mikesmallhelp@gmail.com>
2024-10-28 19:28:19 +0200

    Working without open ai api

--- e2e-tests/test.spec.ts
+++ e2e-tests/test.spec.ts
@@ -500,11 +500,11 @@ const waitForTimeout = async ({ page }: { page: Page }) => {
     await page.waitForTimeout(wait);
 }
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
-
 const checkPageWithAi = async (pageContent: string): Promise<string> => {
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    
     const response = await openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
         messages: [
