09228564f120256efdceebc87aea0d18ac25c48e
Mika Tapanainen <63582395+mikesmallhelp@users.noreply.github.com>
2025-01-30 20:01:55 +0200

    Fix the url forward bug (#65)
    * Fix the url forward bug
    
    * Free tier is slow comment

--- src/commands/test/index.ts
+++ src/commands/test/index.ts
@@ -6,7 +6,7 @@ export default class TestCommand extends Command {
     url: Args.string({description: 'Application url to test, for example: http://localhost:3000', required: true}),
   }
 
-  static description = 'Test any web application. Version 1.5.0.'
+  static description = 'Test any web application with wacat. The current version is 1.5.1.'
 
   static examples = [
     `$ wacat test http://localhost:3000`,
